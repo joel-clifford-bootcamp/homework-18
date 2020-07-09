29f2f61499d8a91ef3ec1ee587867aaa9ee53961
Joel Clifford <joel.clifford@icloud.com>
2020-07-09 17:35:07 -0400

    removed serviceworker and webpack files

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    entry: "./public/index.js",
-    output: {
-      path: __dirname + "/public/dist",
-      filename: "bundle.js"
-    },
-    mode: "development"
-  };
-  module.exports = config;
